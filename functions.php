4fdabadc74667a0d8a447d1992d62e88f0ad06b7
sarbot <sarbot@finchen.localdomain>
2017-03-15 01:48:26 +0100

    some template changes

--- functions.php
+++ functions.php
@@ -1,14 +1,28 @@
 <?php
-function my_theme_enqueue_styles() {
+// Add your custom functions here
 
-    $parent_style = 'sparkling-style'; // This is the main css for the Sparkling theme.
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+function my_child_theme_setup() {
+    add_image_size( 'header', 1200, 400, true );
+    add_image_size('thumb', 200, 200, true);
+}
+add_action( 'after_setup_theme', 'my_child_theme_setup' );
+
+
+
+// Queue parent style followed by child/customized style
+add_action( 'wp_enqueue_scripts', 'sparkling_enqueue_child_styles', 99);
+
+function sparkling_enqueue_child_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_dequeue_style('sparkling-style');
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
+        array('parent-style')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+
+
 ?>
