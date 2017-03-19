f4cc20338763a6cec6ecde6061aeea46efd52666
Michael Rabe <michael_rabe@hotmail.de>
2017-03-19 21:54:41 +0100

    new

---
+++ page-games.php
@@ -0,0 +1,51 @@
+<?php
+ /*
+  Template Name: Games Page Template
+ */
+
+get_header();
+echo 'test1';
+?>
+
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+        
+      <header class="entry-header">
+        <?php the_post_thumbnail(); ?>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+      </header>
+
+      <div class="entry-content">
+        <?php the_content(); ?>
+        <?php
+          $args = array(
+            'post_type' => 'game', // enter custom post type
+            'orderby' => 'date',
+            'order' => 'DESC',
+          );
+              
+          $loop = new WP_Query( $args );
+          if( $loop->have_posts() ):
+          echo 'test';
+          while( $loop->have_posts() ): $loop->the_post(); global $post;
+            echo '<div class="game-entry post-inner-content">';
+            echo '<h4><a href="'.get_the_permalink().'">' . get_the_title() . '</a></h4>';
+            //echo '<div class="portfolio-image">'. get_the_post_thumbnail( $id ).'</div>';
+            //echo '<div class="portfolio-work">'. get_the_content().'</div>';
+            echo '</div>';
+          endwhile;
+          endif;
+        ?>
+      </div><!-- #entry-content -->
+      <?php comments_template( '', true ); ?>               
+    <?php endwhile; // end of the loop. ?>                
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php 
+//get_sidebar();
+get_footer();
+?>
