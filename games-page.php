28afbec6001aa12c1844b964d99d146f90b3664d
Lukas <sarbot@users.noreply.github.com>
2017-03-19 19:56:23 +0100

    Update games-page.php

--- games-page.php
+++ games-page.php
@@ -30,7 +30,7 @@ get_header();
           echo 'test';
           while( $loop->have_posts() ): $loop->the_post(); global $post;
             echo '<div class="game-entry well">';
-            echo '<strong>' . get_the_title() . '</strong>';
+            echo '<h4><a href="'.get_the_permalink().'">' . get_the_title() . '</a></h4>';
             //echo '<div class="portfolio-image">'. get_the_post_thumbnail( $id ).'</div>';
             //echo '<div class="portfolio-work">'. get_the_content().'</div>';
             echo '</div>';
