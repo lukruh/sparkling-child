f8e2b9132496a01b3271b650b8c1e920d4b0ac6b
Lukas <sarbot@users.noreply.github.com>
2017-04-19 09:56:55 +0200

    moved col div to inner posts not page

--- page-games.php
+++ page-games.php
@@ -10,7 +10,7 @@ get_header();
 <main id="main" class="site-main" role="main">
 
     <?php while ( have_posts() ) : the_post(); ?>
-        <div class=col-md-4>
+        
       <header class="entry-header">
         <?php the_post_thumbnail(); ?>
         <h1 class="entry-title"><?php the_title(); ?></h1>
@@ -28,6 +28,7 @@ get_header();
           $loop = new WP_Query( $args );
           if( $loop->have_posts() ):
           while( $loop->have_posts() ): $loop->the_post(); global $post;
+	      <div class=col-md-4>
 		$custom_fields = get_post_custom();
 		echo '<a href="'.$custom_fields['wpcf-apk-url'][0].'">apk</button></a>';
             echo '<div class="game-entry post-inner-content">';
@@ -35,12 +36,13 @@ get_header();
             //echo '<div class="portfolio-image">'. get_the_post_thumbnail( $id ).'</div>';
             //echo '<div class="portfolio-work">'. get_the_content().'</div>';
             echo '</div>';
+	      
+	</div>
           endwhile;
           endif;
         ?>
       </div><!-- #entry-content -->
       <?php comments_template( '', true ); ?>
-	</div>
     <?php endwhile; // end of the loop. ?>                
   </main><!-- #main -->
 </div><!-- #primary -->
